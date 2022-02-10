0a79df4498b59b1e28621df8851c3ab2bdfd9eae
Hayden Smith <hayden.smith-25@outlook.com>
2022-02-10 17:55:02 -0600

    ya we tried to scrape tweets

--- index.js
+++ index.js
@@ -5,4 +5,24 @@ const express = require("express");
 
 const app = express();
 
+const url = "https://twitter.com/home";
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const tweets = [];
+    $(".css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0", html).each(
+      function () {
+        const title = $(this).text();
+        const text = $(this).find("span").attr("span");
+        tweets.push({
+          title,
+          text,
+        });
+      }
+    );
+  })
+  .catch((err) => console.log(err));
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
